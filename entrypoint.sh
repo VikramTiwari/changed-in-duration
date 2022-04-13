3837ea715ab543d87152c728b55420b9d19cd667
Vikram Tiwari <vikramtheone1@gmail.com>
2022-04-13 16:36:51 -0700

    fix: CVE-2022-24765

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 DURATION="${1-.}" # 1day == 86400
 CHANGED=false     # false by default
-echo "Checking if last commit on current branch was less than ${DURATION}seconds ago"
+echo "Checking if last commit on current branch was less than ${DURATION} seconds ago"
+git config --global --add safe.directory /github/workspace # fix for https://github.blog/2022-04-12-git-security-vulnerability-announced/
 declare -i latest=$(git show --format="%ct" HEAD | head -n 1) against=$(date +%s)-${DURATION}
 if [ "$against" -lt "$latest" ]; then CHANGED=true; fi
 
